d6546d844f92b77d34637a65a9435cc5369e59dd
Francisco Josafá <josafa.lopes@gmail.com>
2021-09-13 15:24:46 -0300

    Initial config to pipeline

--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -1,13 +1,15 @@
 package br.com.sw2you.realmeet;
 
+
 import br.com.sw2you.realmeet.api.facade.RoomsApi;
 import br.com.sw2you.realmeet.api.model.Room;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.CompletableFuture;
 
-import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 @RestController
 public class RoomController implements RoomsApi {
