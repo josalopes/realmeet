1972c0b34d0a4153e6ca1335bcd0f1984af0eff1
Francisco Josafá <josafa.lopes@gmail.com>
2021-09-29 08:10:04 -0300

    Configuring tests (unit and integration). Integrations test not still working.

---
+++ service/src/test/java/br/com/sw2you/realmeet/integration/RoomApiIntegrationTest.java
@@ -0,0 +1,55 @@
+package br.com.sw2you.realmeet.integration;
+
+import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
+import static br.com.sw2you.realmeet.utils.TestDataCreator.newRoomBuilder;
+import static org.junit.jupiter.api.Assertions.*;
+
+import br.com.sw2you.realmeet.api.facade.RoomApi;
+import br.com.sw2you.realmeet.core.BaseIntegrationTest;
+import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.HttpClientErrorException;
+
+class RoomApiIntegrationTest extends BaseIntegrationTest {
+    @Autowired
+    private RoomApi roomApi;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Override
+    protected void setupEach() throws Exception {
+        setLocalHostBasePath(roomApi.getApiClient(), "/v1");
+    }
+
+    @Test
+    void testGetRoomSuccess() {
+        var room = newRoomBuilder().build();
+        roomRepository.saveAndFlush(room);
+
+        assertNotNull(room.getId());
+        assertNotNull(room.getActive());
+
+        var dto = roomApi.getRoom(room.getId());
+
+        assertEquals(room.getId(), dto.getId());
+        assertEquals(room.getName(), dto.getName());
+        assertEquals(room.getSeats(), dto.getSeats());
+    }
+
+    @Test
+    public void testGetRoomInactive() {
+        var room = newRoomBuilder().active(false).build();
+        roomRepository.saveAndFlush(room);
+
+        Assertions.assertFalse(room.getActive());
+        assertThrows(HttpClientErrorException.NotFound.class, () -> roomApi.getRoom(room.getId()));
+    }
+
+    @Test
+    public void testGetRoomDoesNotExist() {
+        assertThrows(HttpClientErrorException.NotFound.class, () -> roomApi.getRoom(DEFAULT_ROOM_ID));
+    }
+}
